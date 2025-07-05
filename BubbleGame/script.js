5e983a6e8587630fef5ce7d496452c5f2b8dacc6
adiotz <75060443+beingadityahere@users.noreply.github.com>
2025-07-05 19:24:30 +0530

    initial commit

---
+++ BubbleGame/script.js
@@ -0,0 +1,52 @@
+var timer = 60;
+var hitval = "";
+var score = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score
+}
+
+function getNewHit(){
+    hitval = Math.floor(Math.random()*10)
+    document.querySelector("#hitval").textContent = hitval  
+}
+
+function makeBubble(){
+    var bbl = "";
+    for (var i=1; i<=96; i++) {
+        var rn = Math.floor(Math.random()*10)
+        bbl += `<div class="bubble">${rn}</div>`;
+    }
+document.querySelector("#panelbottom").innerHTML = bbl;
+}
+
+function runTimer(){
+    var timerInt = setInterval(function(){
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else{
+            clearInterval(timerInt)
+            document.querySelector("#panelbottom").innerHTML = "<h1>Game Over</h1>"
+        }    
+    }, 1000)
+
+}
+
+document.querySelector("#panelbottom").addEventListener("click", function (dets){
+    var clickNum = Number(dets.target.textContent)
+    if (clickNum === hitval){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+}
+);
+
+runTimer();
+makeBubble();
+getNewHit();
+
+
